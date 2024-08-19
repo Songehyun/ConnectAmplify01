4f94f38b250f4ef69460f665e639ec7c412b92df
송이현 <songehyun00@gmail.com>
2024-08-19 12:10:17 +0900

    버튼에 테일윈드css 추가

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,25 +1,30 @@
-'use client';
+'use client'; // 클라이언트 전용 컴포넌트
 
 import { useEffect } from 'react';
 
 export default function Home() {
   useEffect(() => {
-    const hello = document.getElementById('hello');
-    hello?.addEventListener('click', () => {
+    const helloButton = document.getElementById('hello');
+
+    const handleClick = () => {
       alert('월드');
-    });
+    };
+
+    helloButton?.addEventListener('click', handleClick);
+
+    return () => {
+      helloButton?.removeEventListener('click', handleClick);
+    };
   }, []);
 
   return (
-    <>
-      <head>
-        <title>Document</title>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </head>
-      <div>
-        <button id="hello">헬로</button>
-      </div>
-    </>
+    <div className="flex justify-center items-center h-screen">
+      <button
+        id="hello"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        헬로
+      </button>
+    </div>
   );
 }
